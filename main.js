4cec0feb57c257ab43af56f74db1ad141e078d23
paronson <paronson@qw-corp.com>
2021-11-26 22:40:27 -0600

    L10-start

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            premium: true
-        }
+            cart: [],
+            premium: true,
+        };
     },
-    methods: {}
-})
+    methods: {
+        addToCart(id) {
+            this.cart.push(id);
+        },
+        removeFromCart(id) {
+            let filteredCart = this.cart.filter((item) => {
+                return item !== id;
+            });
+
+            this.cart = filteredCart;
+        },
+    },
+});
